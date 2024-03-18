52e5289dcc8b7bdd936f255adee03030bf5156ea
Placeblock <felix.weglehner@gmx.de>
2024-03-18 21:36:20 +0100

    Removed old comments

--- client/app.js
+++ client/app.js
@@ -9,34 +9,3 @@ const ship = new Ship(shipElement, {x: 2, y: 2}, {x: 1, y: 0}, 3)
 const ship2 = new Ship(shipElement2, {x: 5, y: 5}, {x: 0, y: 1}, 4)
 field.addShip(ship)
 field.addShip(ship2)
-/*
-shipElement.ondragstart = onDragStart;
-function onDragStart(ev) {
-    const dx = Math.floor(ev.layerX / 40);
-    const dy = Math.floor(ev.layerY / 40);
-    ev.dataTransfer.setData("drag-delta-x", dx)
-    ev.dataTransfer.setData("drag-delta-y", dy)
-    ev.dataTransfer.dropEffect = "move";
-    ev.dataTransfer.setData("text/plain", ev.target.id);
-    requestAnimationFrame(() => {
-        ev.target.style.display = "none"
-    })
-}
-shipElement.ondragend = onDragEnd;
-function onDragEnd(ev) {
-    ev.target.style.display = "block"
-}
-
-function onDrop(ev) {
-    ev.preventDefault();
-    const dx = parseInt(ev.dataTransfer.getData("drag-delta-x"))
-    const dy = parseInt(ev.dataTransfer.getData("drag-delta-y"))
-    const x = parseInt(ev.target.getAttribute("cell-x"))
-    const y = parseInt(ev.target.getAttribute("cell-y"))
-    const targetX = x-dx;
-    const targetY = y-dy;
-    const targetElement = getCell(targetX, targetY);
-    const data = ev.dataTransfer.getData("text/plain");
-    const src = document.getElementById(data);
-    targetElement.appendChild(src);
-}*/
